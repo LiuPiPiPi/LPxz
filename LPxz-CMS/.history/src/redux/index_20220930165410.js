ed3fdd65140e28f49616d0584a24c20abdc83a8a
LiuPiPiPi <liu_bihao@163.com>
2022-09-30 21:41:22 +0800

    init CMS v2

--- LPxz-CMS/.history/src/redux/index_20220930165410.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'react-redux';
-import reducer from './reducer';
-
-// ==============================|| REDUX - MAIN STORE ||============================== //
-
-const store = createStore(reducer);
-const persister = 'Free';
-
-export { store, persister };
